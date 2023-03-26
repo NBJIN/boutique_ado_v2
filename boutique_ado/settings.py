a159a81e5a4e02225e4df91dea1426b6dac73d42
noreen <noreennelligancilms@gmail.com>
2023-03-26 22:26:01 +0000

    update settings urls index and base

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -69,7 +69,7 @@ TEMPLATES = [
         ],
         'APP_DIRS': True,
         'OPTIONS': {
-            'context_processors': [
+                'context_processors': [
                 'django.template.context_processors.debug',
                 'django.template.context_processors.request',  # required by allauth
                 'django.contrib.auth.context_processors.auth',
@@ -109,7 +109,7 @@ WSGI_APPLICATION = 'boutique_ado.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': os.path.join(BASE_DIR , 'db.sqlite3'),
     }
 }
 
--- home/urls.py
+++ home/urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.urls import path
-from . import views  # have to import views in order to get views up and running
+from . import views  # have to import views in order to get views up and runing
 
 
 urlpatterns = {
